f91c80d62747b9c558d960b4ad9ec44491ae5730
jatin kumar <jatins9227@gmail.com>
2023-01-23 17:28:13 +0530

    final

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Voyage.vue'
+import test from '../views/test.vue'
 
 const routes = [
   {
@@ -16,6 +17,14 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
     }
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: test
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
   }
 ]
 
---
+++ src/views/test.vue
@@ -0,0 +1,58 @@
+<script>
+import JsonData from "../../user.json";
+var status = false;
+export default {
+  data() {
+    return {
+      Dropdown: status,
+      data: JsonData,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="customDropdown">
+    <div @click="Dropdown = !Dropdown" class="dropdownHeader">
+      <span>Form Menu</span>
+      <i v-if="Dropdown" class="fa-solid fa-chevron-up drop-icon"> </i>
+      <i v-else class="fa-solid fa-chevron-down drop-icon"> </i>
+    </div>
+    <div v-if="Dropdown" class="dropdownItems">
+      <ul class="dropdownList">
+        <li v-for="subitem in data.sidebar[4].subMenu">
+          <button class="sub-btn sub-dropdown-btn">
+            <span
+              ><img :src="subitem.iconPath" alt="" /> {{ subitem.name }}</span
+            >
+            <i class="fa-solid fa-chevron-down drop-icon"></i>
+          </button>
+
+          <ul>
+            <li>hello</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style>
+.dropdownHeader {
+  width: 230px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  background-color: red;
+  padding: 12px 12px 12px 24px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  color: #303133;
+}
+.dropdownList {
+  list-style-type: none;
+}
+</style>
